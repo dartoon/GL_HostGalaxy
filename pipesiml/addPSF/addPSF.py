015b7cafb2382d6ab36579b23162367786702ead
dartoon <dingxuheng@mail.bnu.edu.cn>
2015-11-20 14:57:21 -0800

    get findpt better

--- pipesiml/addPSF/addPSF.py
+++ pipesiml/addPSF/addPSF.py
@@ -1,21 +1,21 @@
 #Copy the image of PSF and lensed imaged from pylens with sub=4
-
+from findpt import *
 import numpy as np
 import pyfits
 filename='sub400.fits'
 psf = pyfits.open(filename)[0].data.copy()
 #psf = psf[20:-20,20:-20]           # if the psf should be shear
-print psf.shape
 psf /= psf.sum()
-filename='HE1104-1.fits'   # take one image
-hs = pyfits.open(filename)[0].data.copy()
-
-x1=57-len(psf)/2     #where to put the psf
-y1=76-len(psf)/2
-x2=142-len(psf)/2
-y2=135-len(psf)/2
-for i in range(len(psf)):
-   for j in range(len(psf)):
-	hs[i+y1,j+x1]+=psf[i,j]*80
-	hs[i+y2,j+x2]+=psf[i,j]*240
-pyfits.PrimaryHDU(hs).writeto('add.fits',clobber=True)
+for l in range(4):
+	filename='HE1104-{0}.fits'.format(l+1)   # take one image
+	hs = pyfits.open(filename)[0].data.copy()
+	(a1,a2),(b1,b2)=findpt(l+1)
+	x1=a1-len(psf)/2     #where to put the psf
+	y1=a2-len(psf)/2
+	x2=b1-len(psf)/2
+	y2=b2-len(psf)/2
+	for i in range(len(psf)):
+   	   for j in range(len(psf)):
+		hs[i+y1,j+x1]+=psf[i,j]*30
+		hs[i+y2,j+x2]+=psf[i,j]*40
+	pyfits.PrimaryHDU(hs).writeto('add-{0}.fits'.format(l+1),clobber=True)
